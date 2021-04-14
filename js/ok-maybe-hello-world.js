59e8362426c82268cd632f7e3e3556922aa5ef0b
Emmanuel Baldwin <emmanuel.o.baldwin@gmail.com>
2021-04-14 09:55:25 -0700

    Add not-hello-world.html

---
+++ js/ok-maybe-hello-world.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// alert("I swear i wont make you see 'hello world'");
+//
+// alert("I hope the dogs dont get out again:/ ");
+
+
+alert("A beautiful flower grow")
+
+alert("A butterfly flies into the grow")
+
+
+alert("sweet nectar food")
+
+var userInput = prompt("what is your favorite movie");.toUpperCase();
+if (userInput==="saw"){
+    alert("lets play a game!");
+}else {
+    alert("no");
+}
+
+
+
+
+
