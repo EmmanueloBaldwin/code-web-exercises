58ed6b8fcc6540e46de33744a2caca22b601b66a
Emmanuel Baldwin <emmanuel.o.baldwin@gmail.com>
2021-04-15 14:38:11 -0700

    first commit

---
+++ js/operators exercise.js
@@ -0,0 +1,9 @@
+"Use Strict"
+
+
+console.log ( 1 + 2 );
+console.log ( "The type is:" + typeof ( 1 + 2 );
+console.log ( 6 % 4 );
+
+console.log('3 + 4 is ' + (3 + 4));
+
---
+++ js/short hand unary and js methods.js
@@ -0,0 +1,53 @@
+"Use strcit";
+
+// SHORTHAND ASSIGNMENT OPERATORS
+
+// += and -=
+
+//var name = "Casey"; // What if I want my full name?
+
+// We could say:
+
+//name = name + "Edwards"; // But this seems a little terse, let's make it shorter
+
+//name += " Edwards";
+
+//console.log(name);
+
+//// MINI EXERCISE
+// // TODO: Refactor the mini exercise we did earlier on arithmetic operators to use variables and their related shorthand assignment operators, then log out the result
+// //  ie:
+// //     var result = 5;
+// //     console.log(result += 1);
+
+
+var workdaysoftheweek = 8;
+workdaysoftheweek -= 3;
+console.log(workdaysoftheweek);
+
+ var workdaysoftheweek = 5;
+workdaysoftheweek *= 1;
+console.log(workdaysoftheweek);
+
+var workdaysoftheweek = 15;
+workdaysoftheweek /= 3;
+console.log(workdaysoftheweek);
+
+var workdaysoftheweek = 2;
+workdaysoftheweek += 3;
+console.log(workdaysoftheweek);
+
+var result = 5;
+console.log(result *= 1);
+
+var result = 3;          
+console.log(result += 2);
+
+var result = 15;
+console.log(result /= 3);
+
+var result = 10;
+console.log(result -= 5);
+
+var result = 29;            
+console.log(result %= 12);
