fe8af69c5b415a09ce3e2ebffdc66542aa2a426e
Emmanuel Baldwin <emmanuel.o.baldwin@gmail.com>
2021-05-21 14:23:13 -0700

    codingRus

---
+++ js/dom-events1.js
@@ -0,0 +1,20 @@
+
+addButtonEvent()
+
+function addButtonEvent() {
+    document.getElementById ( "submitBtn" )
+        .addEventListener ( "click", addPost )
+}
+
+function addPost() {
+    let textBox = document.getElementById ('blog-container')
+    let textInput = document.getElementById ('user-input')
+    let paragraph = document.createElement("p")
+        textBox.appendChild(paragraph)
+            paragraph.innerText = textInput.value
+}
+
+
+
+
+
